65388bb407b7bc2a9d7edc7e56723eec376fe78d
류원 <2145007@ewha.ac.kr>
2025-10-27 16:43:49 +0900

    chore: config  remotePatterns 추가

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,14 @@ const nextConfig: NextConfig = {
     formats: ["image/webp", "image/avif"], // 자동 WebP/AVIF 변환
     deviceSizes: [640, 750, 828, 1080, 1200, 1920],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "studiox3bucket.s3.ap-northeast-2.amazonaws.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
   },
 
   // 압축 활성화
