e687d5f3e983238b7ef02325c825caf4aee554f3
chaeyoungwon <epopcy@naver.com>
2026-01-06 17:15:26 +0900

    feat: 추천 아이디어 섹션 UI 추가

--- src/app/dashboard/studio/page.tsx
+++ src/app/dashboard/studio/page.tsx
@@ -10,9 +10,25 @@ const StudioPage = () => {
 
   return (
     <div className="flex justify-center">
-      <main>
-        <TabPanel activeTab={activeTab} onChange={setActiveTab} />
-        <TabContent activeTab={activeTab} />
+      <main className="flex items-center mt-[2.81rem]">
+        <div className="flex flex-col">
+          <TabPanel activeTab={activeTab} onChange={setActiveTab} />
+          <TabContent activeTab={activeTab} />
+        </div>
+
+        <section className="w-[36.875rem] h-[40.375rem] flex items-center justify-center bg-Grey-800/75 rounded-lg ml-[1.75rem]">
+          <div className="flex flex-col gap-3 items-center">
+            <h2 className="Subhead_1_semibold text-Grey-300">추천 아이디어</h2>
+            <div className="h-6 w-px bg-Grey-300" />
+            <div className="flex flex-col gap-1 text-Grey-400 Body_2_medium items-center">
+              <span>제품을 강조한 고급 프로모션 이미지</span>
+              <span>
+                소셜 미디어 캠페인을 위한 라이프스타일 컨텍스트 이미지
+              </span>
+              <span>달에서 촬영한 자동차 광고 이미지</span>
+            </div>
+          </div>
+        </section>
       </main>
     </div>
   );
