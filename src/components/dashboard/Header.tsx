38a2b036699a88c4f9d356d8747ad729233cf347
류원 <2145007@ewha.ac.kr>
2026-01-06 17:15:05 +0900

    feat : 헤더 user 팝업 모달 구현

--- src/components/dashboard/Header.tsx
+++ src/components/dashboard/Header.tsx
@@ -18,6 +18,7 @@ type TabName = keyof typeof TAB_CONFIG;
 
 const Header = ({ back = false, tap = false, video = false }: HeaderProps) => {
   const [activePage, setActivePage] = useState<TabName>("스튜디오");
+  const [userIsOpen, setUserIsOpen] = useState(false);
   const router = useRouter();
   const pathname = usePathname();
 
@@ -34,10 +35,18 @@ const Header = ({ back = false, tap = false, video = false }: HeaderProps) => {
   };
 
   return (
-    <header className="px-[6.125rem] pt-5 py-3 flex">
+    <header className="px-[6.125rem] pt-5 py-3 flex relative">
       <div className="flex gap-4 items-center">
-        {back && <Back className="w-[1.5625rem] h-[1.5625rem] " />}
-        <Logo className="w-[5.3125rem]" />
+        {back && (
+          <Back
+            className="w-[1.5625rem] h-[1.5625rem] cursor-pointer"
+            onClick={() => router.back()}
+          />
+        )}
+        <Logo
+          className="w-[5.3125rem] cursor-pointer"
+          onClick={() => router.push("/dashboard")}
+        />
       </div>
 
       {/* 탭  */}
@@ -67,12 +76,52 @@ const Header = ({ back = false, tap = false, video = false }: HeaderProps) => {
       <div className="flex gap-10 items-center">
         {/* 사용방법 */}
         {video && (
-          <div className="flex gap-1 items-center">
+          <button type="button" className="flex gap-1 items-center">
             <Video className="w-7 h-7" />
             <span className="Body_1_medium text-Grey-400">사용방법</span>
-          </div>
+          </button>
         )}
-        <Person className="w-[1.16875rem] h-[1.16875rem]" />
+        <div
+          className="relative"
+          onMouseEnter={() => setUserIsOpen(true)}
+          onMouseLeave={() => setUserIsOpen(false)}
+        >
+          <Person
+            className="w-[1.16875rem] h-[1.16875rem] cursor-pointer"
+            onClick={() => setUserIsOpen((pre) => !pre)}
+          />
+
+          {userIsOpen && (
+            <div className="absolute top-full right-0 pt-6">
+              <div className="px-3 py-4 bg-[rgba(40,44,52,0.90)] rounded-[8px] flex flex-col gap-3 Caption_medium text-Grey-100 min-w-[228px] transition-opacity">
+                <div className="px-5 py-2">cnskdjnksc@gmail.com</div>
+                <div className=" Body_2_medium text-Grey-300 flex flex-col gap-14 ">
+                  <div className="border-t-Grey-500 border-t pt-2">
+                    <button
+                      type="button"
+                      className="py-2 pl-5 w-full text-left"
+                      onClick={() => router.push("/dashboard/mypage")}
+                    >
+                      설정
+                    </button>
+                    <button
+                      type="button"
+                      className="py-2 pl-5 w-full text-left"
+                    >
+                      플랜 관리
+                    </button>
+                  </div>
+                  <button
+                    type="button"
+                    className="pt-4 pb-2 pl-5 border-t-Grey-500 border-t w-full text-left"
+                  >
+                    로그아웃
+                  </button>
+                </div>
+              </div>
+            </div>
+          )}
+        </div>
       </div>
     </header>
   );
---
+++ src/components/dashboard/SideBar.tsx
@@ -0,0 +1,11 @@
+export default function SideBar() {
+  return (
+    <aside className="bg-Grey-800">
+      <div>
+        <div>대시보드</div>
+        <div>프로젝트</div>
+      </div>
+      <div></div>
+    </aside>
+  );
+}
