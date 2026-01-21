baf17776ad04a010a6d5f199f37bace5f6181a21
chaeyoungwon <epopcy@naver.com>
2026-01-21 14:07:08 +0900

    refactor: 폰트 위치 변경

--- src/app/font.ts
+++ src/app/font.ts
@@ -4,17 +4,17 @@ import { Cal_Sans } from "next/font/google";
 export const pretendard = localFont({
   src: [
     {
-      path: "./fonts/Pretendard-Regular.woff2",
+      path: "../../public/fonts/Pretendard-Regular.woff2",
       weight: "400",
       style: "normal",
     },
-    { path: "./fonts/Pretendard-Medium.woff2", weight: "500", style: "normal" },
+    { path: "../../public/fonts/Pretendard-Medium.woff2", weight: "500", style: "normal" },
     {
-      path: "./fonts/Pretendard-SemiBold.woff2",
+      path: "../../public/fonts/Pretendard-SemiBold.woff2",
       weight: "600",
       style: "normal",
     },
-    { path: "./fonts/Pretendard-Bold.woff2", weight: "700", style: "normal" },
+    { path: "../../public/fonts/Pretendard-Bold.woff2", weight: "700", style: "normal" },
   ],
   variable: "--font-pretendard",
   display: "swap",
