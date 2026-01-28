0a5c02144f0c17b6b5914de5a6577e15e4366909
chaeyoungwon <epopcy@naver.com>
2026-01-28 15:58:29 +0900

    design: relative 속성 명시

--- src/components/common/MobileModal.tsx
+++ src/components/common/MobileModal.tsx
@@ -25,7 +25,7 @@ const MobileModal = () => {
 
   return (
     <ModalOverlay className="bg-Black/70" onClose={handleClose}>
-      <div className="bg-Grey-700/90 w-80 rounded-lg flex flex-col items-center gap-11 shadow-[0_0px_12px_rgba(8,8,8,0.25)] backdrop-blur-[5px]">
+      <div className="bg-Grey-700/90 relative w-80 rounded-lg flex flex-col items-center gap-11 shadow-[0_0px_12px_rgba(8,8,8,0.25)] backdrop-blur-[5px]">
         <button
           onClick={handleClose}
           className="absolute top-4 right-4"
@@ -33,7 +33,7 @@ const MobileModal = () => {
         >
           <Close className="w-6 h-6" />
         </button>
-        
+
         <h1 className="Subhead_2_semibold text-Grey-100 mt-[3.25rem] text-center">
           원활한 이용을 위해
           <br />
