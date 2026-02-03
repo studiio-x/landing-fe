ae390c2d60e9e8df66ebf64f6df52270d9a30f49
chaeyoungwon <epopcy@naver.com>
2026-02-03 16:41:55 +0900

    feat: 로그인 모달 백버튼 및 영문 버전 추가

--- src/app/[locale]/login/page.tsx
+++ src/app/[locale]/login/page.tsx
@@ -1,18 +1,20 @@
 "use client";
 
-import { Google, Logo } from "@/assets/icons";
+import { Close, Google, Logo } from "@/assets/icons";
 import LoginInput from "@/components/dashboard/login/LoginInput";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { PATHS } from "@/constants/common/paths";
 import Header from "@/components/dashboard/Header";
 import GlassButton from "@/components/common/GlassButton";
+import { useTranslations } from "next-intl";
 
 export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [isPasswordOpen, setIsPasswordOpen] = useState(false);
   const router = useRouter();
+  const t = useTranslations("login");
 
   const onClick = () => {
     setIsPasswordOpen(!isPasswordOpen);
@@ -24,37 +26,50 @@ export default function Login() {
 
       <main className="flex flex-col items-center justify-center flex-1 pb-[var(--header-height)]">
         <div className="h-fit max-w-[28.75rem] pt-10 pb-12 px-[3.25rem] border-[1.5px] rounded-[0.5rem] border-[rgba(255,48,48,0.35)] bg-Grey-900 shadow-[0_0_8px_0_rgba(255,82,82,0.10),0_0_20px_0_rgba(8,8,8,0.12)] relative gap-[2.5rem] flex flex-col">
+          <button
+            onClick={() => router.back()}
+            className="absolute top-5 right-5"
+            aria-label={t("closeLabel")}
+          >
+            <Close className="w-6 h-6" />
+          </button>
+
           <div className="flex items-center gap-[0.88rem]">
-            <Logo className="w-[7.2rem] h-[2.875rem]" />
-            <span className="Heading_3_semibold text-Grey-50">로그인하기</span>
+            <span className="Heading_3_semibold text-Grey-50">
+              {t.rich("title", {
+                brand: (chunks) => (
+                  <span className="font-calSans text-[1.75rem] font-normal mr-[0.875rem]">{chunks}</span>
+                ),
+              })}
+            </span>
           </div>
 
           <div className="flex flex-col gap-[1.75rem]">
             <button
               type="button"
-              className="bg-Grey-700 rounded-[0.25rem] flex py-[0.75rem] px-[6.53rem] items-center gap-[0.75rem]"
+              className="bg-Grey-700 rounded-[0.25rem] flex py-[0.75rem] w-full justify-center items-center gap-[0.75rem]"
             >
               <Google className="w-[1.75rem] h-[1.75rem]" />
-              <span className="text-Grey-100">구글로 계속하기</span>
+              <span className="text-Grey-100">{t("googleContinue")}</span>
             </button>
 
             <div className="w-full flex gap-[0.75rem] items-center">
               <div className="flex-1 bg-Grey-600 w-full h-[0.0625rem]"></div>
-              <div className="text-Grey-400 Body_1_medium ">또는</div>
+              <div className="text-Grey-400 Body_1_medium">{t("or")}</div>
               <div className="flex-1 bg-Grey-600 w-full h-[0.0625rem]"></div>
             </div>
 
             <form action="">
               <div className="flex flex-col gap-3 mb-7">
                 <LoginInput
-                  placeholder="이메일 주소"
-                  ariaLabel="이메일"
+                  placeholder={t("emailPlaceholder")}
+                  ariaLabel={t("emailAriaLabel")}
                   value={email}
                   onChange={setEmail}
                 />
                 <LoginInput
-                  placeholder="비밀번호"
-                  ariaLabel="비밀번호"
+                  placeholder={t("passwordPlaceholder")}
+                  ariaLabel={t("passwordAriaLabel")}
                   watchIcon={true}
                   isPasswordOpen={isPasswordOpen}
                   onClick={onClick}
@@ -69,21 +84,23 @@ export default function Login() {
                 className="Body_2_semibold"
                 disabled={!email || !password}
               >
-                로그인
+                {t("submit")}
               </GlassButton>
 
               <div className="text-center flex flex-col gap-2 mt-3">
                 <button className="Body_3_medium text-Grey-200 underline">
-                  비밀번호를 잊으셨나요?
+                  {t("forgotPassword")}
                 </button>
                 <button
                   className="flex gap-2 justify-center"
                   onClick={() => router.push(PATHS.SIGNUP)}
                 >
                   <span className="Body_3_regular text-Grey-400">
-                    계정이 없으신가요?
+                    {t("noAccount")}
                   </span>
-                  <div className="Body_3_semibold text-Grey-200">회원가입</div>
+                  <div className="Body_3_semibold text-Grey-200">
+                    {t("signup")}
+                  </div>
                 </button>
               </div>
             </form>
