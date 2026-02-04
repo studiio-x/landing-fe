e1aafcc71552ee26ce59cdced3375aabce2dd2ae
chaeyoungwon <epopcy@naver.com>
2026-02-04 16:46:35 +0900

    refactor: 상수 사용

--- src/components/signup/EmailSentStep.tsx
+++ src/components/signup/EmailSentStep.tsx
@@ -4,15 +4,16 @@ import { useEffect, useState } from "react";
 import { useTranslations } from "next-intl";
 import GlassButton from "@/components/common/GlassButton";
 import type { EmailSentStepProps } from "@/types/signup/funnel.type";
+import { CHANNEL_NAME, MESSAGE_TYPE } from "@/constants/signup/funnel";
 
 const EmailSentStep = ({ email, onNext }: EmailSentStepProps) => {
   const t = useTranslations("signup.emailSent");
   const [isVerified, setIsVerified] = useState(false);
 
   useEffect(() => {
-    const channel = new BroadcastChannel("signup-verification");
+    const channel = new BroadcastChannel(CHANNEL_NAME);
     channel.onmessage = (event) => {
-      if (event.data.type === "email-verified") {
+      if (event.data.type === MESSAGE_TYPE) {
         setIsVerified(true);
         onNext();
       }
