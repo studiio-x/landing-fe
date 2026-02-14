05802763701477f03279753b4f64fdd624d67d99
chaeyoungwon <epopcy@naver.com>
2026-02-14 13:20:47 +0900

    feat: 프로젝트 이어하기 폴더 디자인 추가

--- src/app/[locale]/dashboard/continue/page.tsx
+++ src/app/[locale]/dashboard/continue/page.tsx
@@ -2,16 +2,21 @@
 
 import { Back } from "@/assets/icons";
 import Header from "@/components/dashboard/Header";
+import FolderItem from "@/components/dashboard/project/FolderItem";
 import { useRouter } from "next/navigation";
 import { useTranslations } from "next-intl";
+import { useState } from "react";
+import { mockData } from "@/mocks/dashboard/continue.mock";
 
 const ContinuePage = () => {
   const router = useRouter();
   const t = useTranslations("dashboard");
+  const [deleteModalOpen, setDeleteModalOpen] = useState(false);
 
   return (
-    <div className="flex flex-col h-screen relative">
+    <div className="flex flex-col min-h-dvh relative">
       <div className="fixed inset-0 bg-[url('/images/dashboard/continue-background.png')]  bg-no-repeat bg-top [background-size:100%_auto] -z-10 pointer-events-none" />
+      <div className="fixed inset-0 bg-black/75 -z-10 pointer-events-none" />
       <Header />
 
       <button
@@ -23,19 +28,25 @@ const ContinuePage = () => {
         <Back className="w-11 h-11" />
       </button>
 
-      <main className="w-full flex flex-col items-center gap-[4.5rem] flex-1 justify-center mb-[6.75rem]">
+      <main className="w-full flex flex-col items-center gap-[4.5rem] flex-1 mt-[8.25rem] mb-[6.75rem]">
         <div className="flex flex-col gap-3 items-center">
           <span className="py-2 px-5 border border-Grey-700 bg-[rgba(255,255,255,0.03)] Body_2_medium text-Grey-100 rounded-[5rem]">
             {t("continue.badge")}
           </span>
-          <h1 className="Heading_1_bold text-White">
-            {t("continue.title")}
-          </h1>
+          <h1 className="Heading_1_bold text-White">{t("continue.title")}</h1>
         </div>
 
         <section aria-label={t("continue.badge")}>
-          <ul className="flex items-center gap-9">
-            {/* 폴더 디자인 확정 후 작업 */}
+          <ul className="grid grid-cols-3 gap-x-9 gap-y-11">
+            {mockData.map((item, index) => (
+              <li key={index}>
+                <FolderItem
+                  lists={item}
+                  index={index}
+                  setDeleteModalOpen={setDeleteModalOpen}
+                />
+              </li>
+            ))}
           </ul>
         </section>
       </main>
---
+++ src/mocks/dashboard/continue.mock.ts
@@ -0,0 +1,47 @@
+export const mockData = [
+  {
+    name: "Handbag",
+    isFolder: true,
+    imageUrl: [1, 2, 3, 4, 5, 6].map(() => "/images/project/mockData.png"),
+  },
+  {
+    name: "Cosmetics Visuals",
+    isFolder: true,
+    imageUrl: [1, 2, 3, 4, 5, 6].map(() => "/images/project/mockData.png"),
+  },
+  {
+    name: "Cosmetics Visuals",
+    isFolder: true,
+    imageUrl: [1, 2, 3, 4, 5, 6].map(() => "/images/project/mockData.png"),
+  },
+  {
+    name: "제목을 입력해주세요",
+    isFolder: false,
+    imageUrl: "/images/project/mockData.png",
+  },
+  {
+    name: "제목을 입력해주세요",
+    isFolder: false,
+    imageUrl: "/images/project/mockData.png",
+  },
+  {
+    name: "제목을 입력해주세요",
+    isFolder: false,
+    imageUrl: "/images/project/mockData.png",
+  },
+  {
+    name: "제목을 입력해주세요",
+    isFolder: false,
+    imageUrl: "/images/project/mockData.png",
+  },
+  {
+    name: "제목을 입력해주세요",
+    isFolder: false,
+    imageUrl: "/images/project/mockData.png",
+  },
+  {
+    name: "제목을 입력해주세요",
+    isFolder: false,
+    imageUrl: "/images/project/mockData.png",
+  },
+];
