156b3edd3aefa81bf62a2a6029ee5e59b25d08d3
chaeyoungwon <epopcy@naver.com>
2026-07-29 19:32:19 +0900

    refactor: 폴더 이름 바꾸기 클릭 시 커서 맨 뒤로 가도록 수정

--- src/components/dashboard/project/FolderItem.tsx
+++ src/components/dashboard/project/FolderItem.tsx
@@ -72,7 +72,11 @@ const FolderItem = ({ lists, index, setDeleteModalOpen, onClick }: FolderItemPro
   useEffect(() => {
     if (renameModalOpen) {
       lastValidValue.current = rename;
-      renameModalRef.current?.focus();
+      const textarea = renameModalRef.current;
+      if (textarea) {
+        textarea.focus();
+        textarea.setSelectionRange(textarea.value.length, textarea.value.length);
+      }
       setIsOpenMeatball(false);
     }
     adjustTextareaHeight();
@@ -111,28 +115,33 @@ const FolderItem = ({ lists, index, setDeleteModalOpen, onClick }: FolderItemPro
               gridTemplateRows: "49px 49px 49px",
             }}
           >
-            {(lists.imageUrl as string[]).map((image, idx) => (
-              <Image
-                src={image}
-                key={idx}
-                alt="대시보드 이미지"
-                width={92}
-                height={92}
-                className={clsx("w-full h-full object-cover rounded-xs", {
-                  "col-span-2 row-span-2": idx === 0,
-                })}
-              />
-            ))}
+            {(lists.imageUrl as string[]).map(
+              (image, idx) =>
+                image && (
+                  <Image
+                    src={image}
+                    key={idx}
+                    alt="대시보드 이미지"
+                    width={92}
+                    height={92}
+                    className={clsx("w-full h-full object-cover rounded-xs", {
+                      "col-span-2 row-span-2": idx === 0,
+                    })}
+                  />
+                ),
+            )}
           </div>
         ) : (
           <div className="w-full h-full relative">
-            <Image
-              src={lists.imageUrl as string}
-              alt="폴더이미지"
-              width={292}
-              height={292}
-              className="object-cover w-73 h-full"
-            />
+            {lists.imageUrl && (
+              <Image
+                src={lists.imageUrl as string}
+                alt="폴더이미지"
+                width={292}
+                height={292}
+                className="object-cover w-73 h-full"
+              />
+            )}
           </div>
         )}
 
